5459cff7311a448cde49fb68fdaa522e406c938c
studyagra1607 <studyagra1607@gmail.com>
2024-09-09 20:24:03 +0530

    Remove Unused Validation Code

--- app/Http/Requests/ListRequest.php
+++ app/Http/Requests/ListRequest.php
@@ -24,7 +24,6 @@ class ListRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // 'name' => 'bail|required|regex:/^[a-zA-Z0-9\s\.\']+$/',
             'file' => [
                 'bail',
                 'required',
@@ -46,7 +45,6 @@ class ListRequest extends FormRequest
     public function messages()
     {
         return [
-            // 'name.regex' => 'The :attributes field must only contain letters and numbers.',
             'file.max' => 'The file field must not be greater than 2MB',
         ];
     }
@@ -57,58 +55,5 @@ class ListRequest extends FormRequest
             
         ];
     }
-
-    protected function prepareForValidation(): void
-    {
-        $this->merge([
-            'name' => trim($this->name),
-        ]);
-    }
-    
-    // public function withValidator($validator)
-    // {
-    //     if ($validator->fails()) {
-    //         return;
-    //     }
-
-    //     $validator->after(function ($validator) {
-    //         if (!$this->file('file')) {
-    //             $validator->errors()->add('file', 'The file is required.');
-    //         } else {
-    //             $this->validateFile($validator);
-    //         }
-    //     });
-    // }
-
-    // protected function validateFile($validator)
-    // {
-    //     $rules = [
-    //         'file' => [
-    //             'bail',
-    //             'required',
-    //             'file',
-    //             'mimes:csv,xls,xlsx',
-    //             'max:2560',
-    //             new CsvRule([
-    //                 'name' => 'bail|required|max:255|regex:/^[a-zA-Z0-9\s\.\']+$/',
-    //                 'email' => 'bail|required|email|unique:users,email|max:255',
-    //                 // 'dob' => 'bail|required|date|before:today',
-    //                 // 'address' => 'bail|required|string|max:255',
-    //                 // 'phone' => 'bail|required|string|min:10|max:15',
-    //                 // '' => 'bail|required|string',
-    //             ])
-    //         ],
-    //     ];
-
-    //     $messages = [
-    //         'file.max' => 'The uploaded file may not be larger than 2MB.',
-    //     ];
-        
-    //     $fileValidator = Validator::make($this->all(), $rules, $messages);
-
-    //     if ($fileValidator->fails()) {
-    //         $validator->errors()->add('file', $fileValidator->errors()->first('file'));
-    //     }
-    // }
     
 }
--- app/Http/Traits/CsvParser.php
+++ app/Http/Traits/CsvParser.php
@@ -4,17 +4,19 @@ namespace App\Http\Traits;
 
 trait CsvParser {
 
-    public function CsvToArray( $file )    {
+    public function CsvToArray($file)
+    {
         $content = file_get_contents($file);
         $rows = array_map('str_getcsv', explode(PHP_EOL, $content));
         $rowKeys = array_shift($rows);
         $formattedData = [];
         foreach ($rows as $row) {
-            if( sizeof($row) == sizeof($rowKeys) )  {
+            if(sizeof($row) == sizeof($rowKeys)){
                 $associatedRowData = array_combine($rowKeys, $row);
-                if (empty($keyField)) {
+                if(empty($keyField)){
                     $formattedData[] = $associatedRowData;
-                } else {
+                }
+                else{
                     $formattedData[$associatedRowData[$keyField]] = $associatedRowData;
                 }
             }
@@ -26,28 +28,21 @@ trait CsvParser {
     // {
     //     $csvString = file_get_contents($file);
 
-    //     // Split the CSV string into lines
     //     $lines = explode("\n", $csvString);
 
-    //     // Initialize an array to hold CSV data
     //     $data = [];
 
-    //     // Loop through each line
     //     foreach ($lines as $line) {
-    //         // Split each line by the comma (CSV delimiter)
     //         $data[] = explode(',', $line);
     //     }
 
     //     return $data;
-        
     // }
 
     public function arrayToCsv($data)
     {
-        // Initialize an empty string to store CSV content
         $csvContent = '';
 
-        // Iterate through each row of data and concatenate it into CSV format
         foreach ($data as $row) {
             $csvContent .= '"'.implode('","', $row).'"'.PHP_EOL;
         }
