f3fa25ed0a37a8bc9a06d28f923c6f42740b501e
studyagra1607 <studyagra1607@gmail.com>
2024-09-24 09:08:01 +0530

    Add ESLint rules

--- eslint.config.js
+++ eslint.config.js
@@ -9,4 +9,16 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  {
+    rules: {
+      // "no-console": "off",
+      // "no-debugger": "off",
+      // "no-undef": "off",
+      // "no-unused-vars": "off", 
+      // "no-redeclare": "off",
+      // "vue/no-unused-vars": "off",
+      // "vue/no-parsing-error": "off",
+      // "vue/multi-word-component-names": "off", 
+    }
+  }
 ];
